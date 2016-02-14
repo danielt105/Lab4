4d0bbaef5c97bc0b3545795a699414bc3562ad46
bclarksoftware <bclarksoftware@gmail.com>
2016-02-13 21:56:40 -0800

    Updating the repo with cool stuff

---
+++ Lab4/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float maxSpeed = 3f;
+	bool facingRight = true;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody2D rigidBody2D = GetComponent<Rigidbody2D> ();
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		rigidBody2D.velocity = new Vector2(move * maxSpeed, rigidBody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
